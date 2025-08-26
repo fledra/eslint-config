3f7088e79d414f4f0a890ec974bae9a3571a1463
Fledra <baran@fledra.dev>
2025-08-26 16:23:18 +0300

    chore: updated deps

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,3 @@ export * from './configs';
 
 export { fledra as default } from './factory';
 export * from './types';
-
-export class test {
-  private a!: string;
-  private b!: string;
-
-  public foo() {
-    return this.b;
-  }
-
-  public bar() {
-    return this.a;
-  }
-}
