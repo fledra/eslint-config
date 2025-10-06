05c6a98439de5d4219b4496caa2bfbf4f5c14c2f
Fledra <baran@fledra.dev>
2025-10-06 17:43:04 +0300

    fix: added imports key to package.json sort

--- src/configs/jsonc.ts
+++ src/configs/jsonc.ts
@@ -130,6 +130,7 @@ export async function sortPackageJson(): Promise<TypedFlatConfigItem[]> {
               'types',
               'typesVersions',
               'files',
+              'imports',
               'exports',
               'homepage',
               'repository',
