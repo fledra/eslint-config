a5d86b546a97b502066ac5010f84f26abb4fede3
Fledra <baran@fledra.dev>
2026-03-08 21:35:11 +0300

    fix: made jsdoc work on source files only

--- src/configs/jsdoc.ts
+++ src/configs/jsdoc.ts
@@ -2,6 +2,8 @@ import type { TypedFlatConfigItem } from '../types';
 
 import pluginJsdoc from 'eslint-plugin-jsdoc';
 
+import { GLOB_SRC } from '../globs';
+
 export interface JSDocOptions {
   /**
    * @default true
@@ -9,37 +11,43 @@ export interface JSDocOptions {
   stylistic?: boolean;
 }
 
-export function jsdoc(options: JSDocOptions = {}): TypedFlatConfigItem {
+export function jsdoc(options: JSDocOptions = {}): TypedFlatConfigItem[] {
   const {
     stylistic = true,
   } = options;
 
-  return {
-    name: 'fledra/jsdoc/rules',
-    plugins: {
-      jsdoc: pluginJsdoc,
+  return [
+    {
+      name: 'fledra/jsdoc/setup',
+      plugins: {
+        jsdoc: pluginJsdoc,
+      },
     },
-    rules: {
-      'jsdoc/check-access': 'warn',
-      'jsdoc/check-param-names': 'warn',
-      'jsdoc/check-property-names': 'warn',
-      'jsdoc/check-types': 'warn',
-      'jsdoc/empty-tags': 'warn',
-      'jsdoc/implements-on-classes': 'warn',
-      'jsdoc/no-defaults': 'warn',
-      'jsdoc/no-multi-asterisks': 'warn',
-      'jsdoc/require-param-name': 'warn',
-      'jsdoc/require-property': 'warn',
-      'jsdoc/require-property-description': 'warn',
-      'jsdoc/require-property-name': 'warn',
-      'jsdoc/require-returns-check': 'warn',
-      'jsdoc/require-returns-description': 'warn',
-      'jsdoc/require-yields-check': 'warn',
+    {
+      name: 'fledra/jsdoc/rules',
+      files: [GLOB_SRC],
+      rules: {
+        'jsdoc/check-access': 'warn',
+        'jsdoc/check-param-names': 'warn',
+        'jsdoc/check-property-names': 'warn',
+        'jsdoc/check-types': 'warn',
+        'jsdoc/empty-tags': 'warn',
+        'jsdoc/implements-on-classes': 'warn',
+        'jsdoc/no-defaults': 'warn',
+        'jsdoc/no-multi-asterisks': 'warn',
+        'jsdoc/require-param-name': 'warn',
+        'jsdoc/require-property': 'warn',
+        'jsdoc/require-property-description': 'warn',
+        'jsdoc/require-property-name': 'warn',
+        'jsdoc/require-returns-check': 'warn',
+        'jsdoc/require-returns-description': 'warn',
+        'jsdoc/require-yields-check': 'warn',
 
-      ...(stylistic && {
-        'jsdoc/check-alignment': 'warn',
-        'jsdoc/multiline-blocks': 'warn',
-      }),
+        ...(stylistic && {
+          'jsdoc/check-alignment': 'warn',
+          'jsdoc/multiline-blocks': 'warn',
+        }),
+      },
     },
-  };
+  ];
 }
